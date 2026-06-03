90ee15b68220f8fca3909ec452ba4ea55e9aabef
Jack Vorfale <Vorfale@gmail.com>
2026-06-03 23:53:05 +0300

    Keep plain chat input with chat settings

--- tweaks/index.js
+++ tweaks/index.js
@@ -1,11 +1,11 @@
 export const TWEAK_MANIFESTS = [
   "tweaks/sr-portraits/tweak.json",
   "tweaks/chat-images/tweak.json",
-  "tweaks/levels/tweak.json",
-  "tweaks/round-token-borders/tweak.json",
   "tweaks/chat-auto-scroll-fix/tweak.json",
+  "tweaks/plain-chat-input/tweak.json",
   "tweaks/sr5-aa-bridge/tweak.json",
+  "tweaks/round-token-borders/tweak.json",
   "tweaks/random-ambient-sounds/tweak.json",
   "tweaks/actor-token-setup/tweak.json",
-  "tweaks/plain-chat-input/tweak.json"
+  "tweaks/levels/tweak.json"
 ];
